8abc37ea4677813fcdba75e6a4fe5b22161b953c
enigma-devhub <emmanuelonoja291@gmail.com>
2024-01-10 15:09:59 +0100

    footer

--- components/Shared/Footer.tsx
+++ components/Shared/Footer.tsx
@@ -1,14 +1,21 @@
+import Link from "next/link"
 import { Button } from "../ui/button"
+import Image from "next/image"
 
 
 const Footer = () => {
   return (
-    <div>
+   
+<footer className="border-t">
+<div className="flex-center wrapper flex-between flex flex-col gap-4 p-5 sm:flex-row text-center">
+  <Link href="/">
+    <Image src="/logo.png" alt="logo" width={129} height={585} />
+  </Link>
+  <p className="text-xs">{new Date().getFullYear()} © Evento. All Rights Reserved.</p>
+ </div>
+</footer>
 
-      <Button>
-        Footer
-      </Button>
-    </div>
+   
   )
 }
 
