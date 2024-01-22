a71714fa62bd1f47f0edb271cde232b6382ef7e3
enigma-devhub <emmanuelonoja291@gmail.com>
2024-01-22 07:43:33 +0100

    database update

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -1,11 +1,30 @@
-// import mongoose from "mongoose"
+// Please this code is a TypeScript module that provides a function (connectToDatabase)
+//  for connecting to a MongoDB database using the Mongoose library.
+//  It utilizes caching to avoid redundant database connections Thank you./ 
 
-// const MONGODB_URL = process.env.MONGODB_URL
+import mongoose from "mongoose";
 
-// let cached = (global as any ).mongoose || {conn: null, promise: null}
+const MONGODB_URL = process.env.MONGODB_URL;
 
-// export const connectToDatabase = async () => {
-//     // check if connection exists 
+let cached = (global as any).mongoose || { conn: null, promise: null };
 
-//     if (cached.conn) return cached.conn;
-// }
+export const connectToDatabase = async () => {
+    // Check if connection exists
+    if (cached.conn) return cached.conn;
+
+    // Check if MONGODB_URL is defined
+    if (!MONGODB_URL) throw new Error("Invalid MongoDB URL");
+
+    // If not connected, establish a new connection
+    cached.promise = cached.promise || mongoose.connect(MONGODB_URL, {
+        dbName: "evento",
+        bufferCommands: false,
+    });
+
+    // Await the connection promise and set it to the cached connection
+    cached.conn = await cached.promise;
+
+    // Return the connection
+    return cached.conn;
+};
+
