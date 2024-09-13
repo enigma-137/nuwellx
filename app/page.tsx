51338b97d81b6b7362391bd7abeecf8817b179d6
enigma-devhub <emmanuelonoja291@gmail.com>
2024-09-13 17:42:20 +0100

    switch

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { Button } from '@/components/ui/button'
 import React, { useRef, useState, useCallback } from 'react'
 import Webcam from 'react-webcam'
 
@@ -31,18 +32,18 @@ export default function Camera({ onCapture }: CameraProps) {
         className="mb-4"
         videoConstraints={{ facingMode }}
       />
-      <button
+      <Button
         onClick={capture}
         className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 mb-2"
       >
         Capture Image
-      </button>
-      <button
+      </Button>
+      <Button variant="ghost"
         onClick={switchCamera}
         className="px-4 py-2 bg-gray-500 text-white rounded hover:bg-gray-600"
       >
         Switch Camera
-      </button>
+      </Button>
     </div>
   )
 }
