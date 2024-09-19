27a6183abcf7084b80c0eb5897cae6a8a8c0773b
enigma-devhub <emmanuelonoja291@gmail.com>
2024-09-19 07:28:59 +0100

    new

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export const metadata: Metadata = {
   title: 'Nuwell',
   description: 'Food and nutrition app',
   icons: {
-    icon: '/favicon.ico', // Path to the favicon
+    icon: '/favicon.ico', 
   },
 }
 
