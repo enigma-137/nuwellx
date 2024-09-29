5b5e5d12215507be4bfbadf3273d5f6697cd9915
enigma-devhub <emmanuelonoja291@gmail.com>
2024-09-29 15:56:47 +0100

    new

--- components/Results.tsx
+++ components/Results.tsx
@@ -9,7 +9,7 @@ interface ResultsProps {
 
 export default function Results({ analysis }: ResultsProps) {
   return (
-    <div className="mt-8 p-4  bg-gray-100 rounded border ">
+    <div className="mt-8 p-4   rounded border ">
       <h2 className="text-xl font-bold mb-4">Analysis Results:</h2>
       <pre className="whitespace-pre-wrap"><ReatMarkDown>{analysis}</ReatMarkDown></pre>
     </div>
